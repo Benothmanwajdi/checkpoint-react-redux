c7fbe824743aea71ddc8b3cd587c052ec42fb64f
Benothmanwajdi <benothmanwajdi025@gmail.com>
2022-03-17 09:33:35 +0100

    react components

---
+++ Desktop/checkpoint-react-compenents/src/App.js
@@ -0,0 +1,17 @@
+import Address from "./components/Profile/Address"
+import FullName from "./components/Profile/FullName"
+import ProfilePhoto from "./components/Profile/ProfilePhoto"
+
+const App = () => {
+  return (
+    <div>
+
+<ProfilePhoto/>
+<FullName/>
+<Address/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Desktop/checkpoint-react-compenents/src/components/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+
+
+        <img  src="" alt="" />
+    </div>
+  )
+}
+
+export default ProfilePhoto
