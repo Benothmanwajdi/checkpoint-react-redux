e5adcd3a141ef83ccd197af0ed6f8e3ddc36c9cd
Benothmanwajdi <benothmanwajdi025@gmail.com>
2022-03-21 10:48:11 +0100

    react-props

--- Desktop/checkpoint-react-props/src/App.js
+++ Desktop/checkpoint-react-props/src/App.js
@@ -1,2 +1,18 @@
-import Data from "./Profile/profile";
-import Image from ""
+import Data from "./Profile/Components";
+import Image from "./icon.jpg"
+
+function App() {
+  
+  let fun = {fullname:"wajdi ben othmen",bio:"Hello world",profession:"webdevelopper"}
+  
+  return (
+    <div className="App">
+      <Data data={fun}>
+        <img width={350} src={Image} />
+        </Data>
+      
+    </div>
+  );
+}
+
+export default App;
